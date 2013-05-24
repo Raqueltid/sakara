0bc822d4851987b33f0417b6555890b366fea8bc
Toni Rodriguez Valiente <arodriguez@amaris.cat>
2013-05-24 13:52:40 +0200

    Create form to add client

--- sakara/sakara/common/views.py
+++ sakara/sakara/common/views.py
@@ -1,9 +1,14 @@
+# -*- coding: utf-8 -*-
+
 from django.views.generic import TemplateView, View
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth import authenticate
 from django.contrib.auth import login
 from django import forms
 from django.contrib import messages
+from sakara.common.models import Clientes
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 
 class LoginForm(forms.Form):
@@ -21,11 +26,26 @@ class LoginForm(forms.Form):
         label="password",
         required=True,
         error_messages={
-            "invalid": "Contrasea invalida",
-            "required": "El campo 'contrasea' es obligatorio",
+            "invalid": "Contraseña invalida",
+            "required": "El campo 'contraseña' es obligatorio",
         },
-        widget=forms.PasswordInput(attrs={'placeholder': 'Introduce la contrasea', 'autocomplete': 'off'})
+        widget=forms.PasswordInput(attrs={'placeholder': 'Introduce la contraseña', 'autocomplete': 'off'})
     )
+
+
+class ClientesForm(forms.ModelForm):
+    class Meta:
+        model = Clientes
+        # Nota: Eliminando el atributo 'fields' anade por defecto todos los elemetnos
+        widgets = {
+            'nombres': forms.TextInput(attrs={'placeholder': 'Nombre'}),
+            'apellidos': forms.TextInput(attrs={'placeholder': 'Apellidos'}),
+            #TODO: Fecha de nacimiento con calendario // Por ahora acepta formato %m/%d%/%Y
+            'fecha_nac': forms.DateInput(format='%d/%m/%Y', attrs={'placeholder': 'Fecha'}),
+            'direccion': forms.TextInput(attrs={'placeholder': 'Dirección'}),
+            'email': forms.TextInput(attrs={'placeholder': 'Email'}),
+            'observaciones': forms.Textarea(attrs={'placeholder': 'Observaciones', 'cols': 50, 'rows': 10}),
+        }
 
 
 class LoginView(TemplateView):
@@ -57,6 +77,23 @@ class LoginView(TemplateView):
 class ConsultaView(TemplateView):
     template_name = "consulta.html"
 
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super(ConsultaView, self).dispatch(*args, **kwargs)
+
     def get_context_data(self, **kwargs):
-        context = {}
+        context = super(ConsultaView, self).get_context_data(**kwargs)
+        context["form"] = ClientesForm()
         return context
+
+    def post(self, request):
+        v = {}
+        form = v["form"] = ClientesForm(data=request.POST)
+
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Cliente anyadido correctamente!")
+            return HttpResponseRedirect('')
+        else:
+            messages.error(request, "No se ha podido anyadir el Cliente.")
+        return self.render_to_response(v)
