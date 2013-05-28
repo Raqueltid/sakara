e84810cab34e688e386ead7bfbc9a1e00d808a23
Toni Rodriguez Valiente <arodriguez@amaris.cat>
2013-05-28 12:35:01 +0200

    Modify Client model, change template names and constant file it's added

--- sakara/sakara/common/urls.py
+++ sakara/sakara/common/urls.py
@@ -16,5 +16,6 @@ urlpatterns = patterns('',
     # url(r'^admin/', include(admin.site.urls)),
     url(r'^$', views.LoginView.as_view()),
     url(r'^login/$', views.LoginView.as_view()),
-    url(r'^consulta/$', views.ConsultaView.as_view())
+    url(r'^home/$', views.HomeView.as_view()),
+    url(r'^client/add/$', views.AddClientView.as_view())
 )
